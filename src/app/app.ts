1455fed23fc2f4ece74b8feae7197bcc129af9e0
Filipe Antunes <filipe-antunes@outlook.com>
2025-07-03 12:07:01 -0300

    Ajuste imports AppComponent

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterModule, RouterOutlet } from '@angular/router';
-import { Home } from './components/home/home';
 import { SidebarComponent } from "./components/sidebar/sidebar";
 import { Header } from './components/header/header';
-import { Cadastrar } from './components/cadastrar/cadastrar';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet,
             RouterModule,
-            Home,
             SidebarComponent,
-            Header,
-            Cadastrar],
+            Header],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
